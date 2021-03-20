6e4568255854748351f3e906c813c86fac822af6
karinasanchez04 <karinasanchez505@yahoo.com>
2021-03-20 13:14:05 -0700

    small formatting changes and functionality of the links

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_screen);
+        setContentView(R.layout.resources_screen);
+
+        //to have the link working for care
+        TextView textCare = (TextView) findViewById(R.id.textView8);
+        textCare.setMovementMethod(LinkMovementMethod.getInstance());
+
+        //caps link
+        TextView textCaps = (TextView) findViewById(R.id.textView10);
+        textCaps.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
