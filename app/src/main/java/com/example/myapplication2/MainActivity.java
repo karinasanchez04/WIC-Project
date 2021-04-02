5907721dc256c457af02b8d27a9421fc13a7ed9f
karinasanchez04 <karinasanchez505@yahoo.com>
2021-04-01 23:59:56 -0700

    created a loading screen and modified settings screen

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ComponentActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
@@ -16,10 +19,31 @@ public class MainActivity extends AppCompatActivity {
     private Button aboutBtn;
     private Button emergencyBtn;
 
+    public static final String FIRST_NAME = "FIRST CONTACT NAME";
+    public static final String SECOND_NAME = "SECOND CONTACT NAME";
+    public static final String FIRST_NUM = "FIRST CONTACT NUM";
+    public static final String SECOND_NUM = "SECOND CONTACT NUM";
+
+    private String firstContactName;
+    private String firstContactNum;
+    private String secondContactName;
+    private String secondContactNum;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE);
+
+        //stores contact info
+        firstContactName = sp.getString("firstContactName", "");
+        secondContactName = sp.getString("secondContactName", "");
+        firstContactNum = sp.getString("firstContactNum", "");
+        secondContactNum = sp.getString("secondContactNum", "");
+
 
         //functionality of buttons on toolbar
         resourcesBtn = findViewById(R.id.button12);
--- app/src/main/java/com/example/myapplication2/SettingsActivity.java
+++ app/src/main/java/com/example/myapplication2/SettingsActivity.java
@@ -3,21 +3,29 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
     private Button resourcesBtn;
     private Button homeBtn;
     private Button aboutBtn;
 
-    private String name;
-    private String firstContactName;
-    private String secondContactName;
-    private String firstContactNum;
-    private String secondContactNum;
+    public String name;
+    public String firstContactName;
+    public String secondContactName;
+    public String firstContactNum;
+    public String secondContactNum;
+
+    public String name_load;
+    public String firstContactName_load;
+    public String secondContactName_load;
+    public String firstContactNum_load;
+    public String secondContactNum_load;
 
     private EditText nameInput;
     private EditText firstContactNameInput;
@@ -25,9 +33,18 @@ public class SettingsActivity extends AppCompatActivity {
     private EditText firstContactNumInput;
     private EditText secondContactNumInput;
 
-    private Button saveNameBtn;
-    private Button saveFirstConBtn;
-    private Button saveSecConBtn;
+    SharedPreferences sp;
+
+    private Button saveAllBtn;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String NAME = "name";
+    public static final String FIRST_CONT_NAME = "firstContactName";
+    public static final String FIRST_CONT_NUM = "firstContactNum";
+    public static final String SEC_CONT_NAME = "secondContactName";
+    public static final String SEC_CONT_NUM = "secondContactNum";
+
+
 
 
     @Override
@@ -52,36 +69,70 @@ public class SettingsActivity extends AppCompatActivity {
         firstContactNumInput = (EditText)findViewById(R.id.firstContactNum);
         secondContactNumInput = (EditText)findViewById(R.id.secondContactNum);
 
-        saveNameBtn = (Button) findViewById(R.id.saveNameBtn);
-        saveNameBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                name = nameInput.getText().toString();
+        saveAllBtn =(Button) findViewById(R.id.saveAll);
 
-            }
-        });
-        saveFirstConBtn =(Button) findViewById(R.id.saveFirstContactBtn);
-        saveFirstConBtn.setOnClickListener(new View.OnClickListener() {
+        sp = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        saveAllBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                name = nameInput.getText().toString();
                 firstContactName = firstContactNameInput.getText().toString();
                 firstContactNum = firstContactNumInput.getText().toString();
-
-
-            }
-        });
-        saveSecConBtn = (Button) findViewById(R.id.saveSecondContactBtn);
-        saveSecConBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
                 secondContactName = secondContactNameInput.getText().toString();
                 secondContactNum = secondContactNumInput.getText().toString();
 
+                saveData();
+                sendData();
+
             }
         });
+
+        loadData();
+        updateViews();
+
+
+    }
+    public void saveData(){
+
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(NAME, name);
+        editor.putString(FIRST_CONT_NAME, firstContactName);
+        editor.putString(SEC_CONT_NAME, secondContactName);
+        editor.putString(FIRST_CONT_NUM, firstContactNum);
+        editor.putString(SEC_CONT_NUM, secondContactNum);
+        editor.commit();
+
+        Toast.makeText(SettingsActivity.this, "Data Saved", Toast.LENGTH_SHORT).show();
     }
 
+    public void loadData(){
+
+        name_load = sp.getString(NAME, "");
+        firstContactName_load = sp.getString(FIRST_CONT_NAME, "");
+        firstContactNum_load = sp.getString(FIRST_CONT_NUM, "");
+        secondContactName_load = sp.getString(SEC_CONT_NAME, "");
+        secondContactNum_load = sp.getString(SEC_CONT_NUM, "");
+    }
+
+    public void  updateViews(){
+        nameInput.setText(name_load);
+        firstContactNameInput.setText(firstContactName_load);
+        firstContactNumInput.setText(firstContactNum_load);
+        secondContactNameInput.setText(secondContactName_load);
+        secondContactNumInput.setText(secondContactNum_load);
+    }
+    public void sendData(){
+
+
+        Intent i = new Intent(SettingsActivity.this, MainActivity.class);
+
+        i.putExtra(MainActivity.FIRST_NAME, firstContactName);
+        i.putExtra(MainActivity.FIRST_NUM, firstContactNum);
+        i.putExtra(MainActivity.SECOND_NAME, secondContactName);
+        i.putExtra(MainActivity.SECOND_NUM, firstContactNum);
+
+    }
     public void openHomeActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
